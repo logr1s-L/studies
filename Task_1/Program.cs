586f9491e7f78d4e756de63d12eab252905a6967
logr1s <denisdoroshenko50@gmail.com>
2026-03-19 19:50:14 +0200

    Add Cmd_Program

---
+++ Task_1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1
+{
+    // Завдання 1
+    // Вивести на екран усі числа від нуля до введеного користувачем числа.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введiть число: ");
+            int a = int.Parse(Console.ReadLine());
+
+            int i = 0;
+
+            for (i = 1; i <=a; i++)
+                {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
