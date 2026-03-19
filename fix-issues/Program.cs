d8221df7c366f3103e7b5cc3c6fbb8dfcdd2f582
logr1s <denisdoroshenko50@gmail.com>
2026-03-19 20:23:13 +0200

    Fix

---
+++ fix-issues/Program.cs
@@ -0,0 +1,20 @@
+namespace Task_5
+{
+    // Завдання 5
+    // Написати програму, яка виводить на екран таблицю множення на k де k – номер варіанта
+    // coments
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введiть число:");
+            int a = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+                Console.Write($"{a} * {i} + = {a * i} \n");
+            Console.WriteLine();
+
+
+        }
+    }
+}
